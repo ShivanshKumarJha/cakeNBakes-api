10d8989baf77abfd2aa8b8aaa6a54027a1b9643f
Shivansh Kumar Jha <kumarjhashivansh@gmail.com>
2026-08-07 01:48:06 +0530

    Created the banner entity

--- src/main/java/com/shivansh/cakes/banner/entity/Banner.java
+++ src/main/java/com/shivansh/cakes/banner/entity/Banner.java
@@ -1,1 +1,28 @@
 package com.shivansh.cakes.banner.entity;
+
+import com.shivansh.cakes.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Banner extends BaseEntity {
+
+    @Column(nullable = false, length = 50)
+    private String banner;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(length = 100)
+    private String caption;
+
+    private String category;
+}
