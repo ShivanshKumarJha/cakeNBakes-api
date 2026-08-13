4a6b27c4a9cf0c59ef9e1918ec699d3b89950353
Shivansh Kumar Jha <kumarjhashivansh@gmail.com>
2026-08-14 01:28:42 +0530

    One:Many Relationship with Cart:CartItems

--- src/main/java/com/shivansh/cakes/cart/entity/Cart.java
+++ src/main/java/com/shivansh/cakes/cart/entity/Cart.java
@@ -2,11 +2,12 @@ package com.shivansh.cakes.cart.entity;
 
 import com.shivansh.cakes.common.entity.BaseEntity;
 import com.shivansh.cakes.user.entity.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -16,4 +17,17 @@ public class Cart extends BaseEntity {
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    public void addCartItem(CartItem cartItem) {
+        cartItems.add(cartItem);
+        cartItem.setCart(this);
+    }
+
+    public void removeCartItem(CartItem cartItem) {
+        cartItems.remove(cartItem);
+        cartItem.setCart(null);
+    }
 }
--- src/main/java/com/shivansh/cakes/cart/entity/CartItem.java
+++ src/main/java/com/shivansh/cakes/cart/entity/CartItem.java
@@ -3,6 +3,9 @@ package com.shivansh.cakes.cart.entity;
 import com.shivansh.cakes.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +14,7 @@ import lombok.Setter;
 @Entity
 public class CartItem extends BaseEntity {
 
+    @Min(value = 1, message = "Quantity must be at least 1")
     @Column(nullable = false)
     private Integer quantity;
 
@@ -22,4 +26,8 @@ public class CartItem extends BaseEntity {
 
     @Column(nullable = false)
     private Double subTotal;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
 }
